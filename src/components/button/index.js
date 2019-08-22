44be3b2edc3c324a16dadd83648c221f22171254
LIUJIA <982094509@qq.com>
2019-08-22 16:56:19 +0800

    提交

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
 
-const Button = ({text}) => (<button className="wm-btn">这是一个按钮{text}</button>)
+const Button = ({text}) => (<button className="wm-btn">这是个按钮{text}</button>)
 
 export default Button;
